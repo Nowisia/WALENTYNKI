7081d235fc3295c13372ebb97dfa002d069223d9
Nowisia <128217165+Nowisia@users.noreply.github.com>
2024-01-31 17:11:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showMessage(response) {
-  if (response === "NIE") {
-    const nieButton = document.getElementById("nie-button");
+  if (response === "No") {
+    const noButton = document.getElementById("no-button");
     const container = document.querySelector(".container");
     const maxWidth = window.innerWidth - noButton.offsetWidth;
     const maxHeight = window.innerHeight - noButton.offsetHeight;
@@ -21,25 +21,25 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "Przycisk "nie" jest tylko dla podpuchy mendo";
+      "The no button is just for visuals";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
   }
 
-  if (response === "TAK") {
-    // Remove name message and nie button
+  if (response === "Yes") {
+    // Remove name message and no button
     document.getElementById("name").remove();
     document.getElementById("no-button").remove();
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    takMessage.textContent = "TAAAAK, do zobaczenia 14 lutego piękny chłopcze😘😘";
-    takMessage.style.display = "block";
-    takMessage.style.fontStyle = "normal";
+    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.style.display = "block";
+    yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
 
-    // Remove tak button
-    document.getElementById("takButton").remove();
+    // Remove yes button
+    document.getElementById("yesButton").remove();
   }
 }
